8f05fc395aad58158ce6e2c68b45ff7c38d7c30b
ValMin-dev <valeriy.minyukov@gmail.com>
2026-03-16 17:35:06 +0200

    begin auth, jwt, dto, decorators

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { AuthDto } from './dto/auth.dto'
+import { UserService } from '../user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private userService: UserService,
+		private jwt: JwtService
+	) {}
+
+	async login(dto: AuthDto) {
+		return dto
+	}
+}
