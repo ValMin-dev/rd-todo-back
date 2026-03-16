bcc81a281d9b18ff2729514ed4fc5560fcab411a
ValMin-dev <valeriy.minyukov@gmail.com>
2026-03-16 20:25:21 +0200

    auth, cookie, login, logout, registration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
+import { AuthDto } from 'src/auth/dto/auth.dto'
+import { hash } from 'argon2'
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
 
-	async getById(id: string) {
-		return this.prisma.user.findUnique({
+	getById(id: string) {
+		return this.prisma.user.findUniqueOrThrow({
 			where: { id },
 			include: { tasks: true }
 		})
+	}
+
+	getByEmail(email: string) {
+		return this.prisma.user.findUnique({ where: { email } })
+	}
+	async create(dto: AuthDto) {
+		const user = {
+			email: dto.email,
+			password: await hash(dto.password)
+		}
+
+		return this.prisma.user.create({ data: user })
 	}
 }
